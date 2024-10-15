e5e5f26e37c06ed4021738db07a99371249095b0
Fidyan-04 <fidyan.techstreat@gmail.com>
2024-10-15 10:36:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleOnClick() {
+    document.getElementById("mobile-nav").style.display = "block";
+    document.getElementById("tgl-img").style.display = "none";
+    document.getElementById("close-btn").style.display = "block";
+}
+function closeOnClick(){
+    document.getElementById("mobile-nav").style.display = "none";
+    document.getElementById("tgl-img").style.display = "block";
+    document.getElementById("close-btn").style.display = "none";
+}
