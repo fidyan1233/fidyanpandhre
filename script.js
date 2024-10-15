a027f4097de0504136ff4484e97d2c96d19c5286
Fidyan-04 <fidyan.techstreat@gmail.com>
2024-10-15 15:25:59 +0530

    added banner

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function toggleOnClick() {
     document.getElementById("mobile-nav").style.display = "block";
     document.getElementById("tgl-img").style.display = "none";
     document.getElementById("close-btn").style.display = "block";
+
+     document.body.style.overflow = "hidden";
+    
 }
 function closeOnClick(){
     document.getElementById("mobile-nav").style.display = "none";
     document.getElementById("tgl-img").style.display = "block";
     document.getElementById("close-btn").style.display = "none";
+     document.body.style.overflow = "auto"; // or "visible"
 }
